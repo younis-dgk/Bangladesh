1aaeffe23f352e7a64e0c2908789a183c29aebb3
GANGSTER <167035490+younis-dgk@users.noreply.github.com>
2025-03-09 08:29:56 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -3,7 +3,7 @@ os.system('git pull --quiet 2>/dev/null')
 os.system("clear")
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import Safeum
+    import R
 elif bit == '32bit':
     print("\033[0m\033[91m Sorry Your Device Is 32bit, This Tool 32bit Not Sported, Only For \033[92m64Bit\033[0m ]");exit() 
  
